cc19128a269de4868736a25efd075b16af7a0d9c
italocirillo <italocirillo@gmail.com>
2023-05-05 22:21:49 +0200

    MILESTONE 1 FATTO

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+$caratteri = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '?', '!', '£', '$', '€', '*', '#', '@', '(', ')', '[', ']', '{', '}'];
+
+if (isset($_GET["lunghezza_password"])) {
+    $lunghezza = $_GET["lunghezza_password"];
+    $password = genera_password($lunghezza, $caratteri);
+}
+
+function genera_password($lunghezza, $caratteri)
+{
+    $password = "";
+    $indice_casuale = 0;
+    for ($i = 0; $i < $lunghezza; $i++) {
+        $indice_casuale = rand(0, count($caratteri) - 1);
+        $password .= $caratteri[$indice_casuale];
+    }
+    return $password;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="it">
 
@@ -28,9 +48,10 @@
                         <label for="lunghezza-password">Lunghezza Password:</label>
                     </div>
                     <div class="sezione-input">
-                        <input type="text" name="lunghezza-password" id="lunghezza-password">
+                        <input type="number" name="lunghezza_password" id="lunghezza-password" min=0 max=26>
                     </div>
                 </div>
+                <h4>password: <?php echo $password ?? isset($_GET["lunghezza_password"]) ?></h4>
                 <button type="submit" class="invia">Invia</button>
                 <button type="reset">Annulla</button>
             </form>
